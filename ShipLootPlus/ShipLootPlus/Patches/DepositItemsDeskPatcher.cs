4810c13d26c8d0823b568569eebfa7103078649c
ProfX <prof@pxcnet.com>
2024-01-29 23:05:41 -0800

    Added two patch methods and event hooks which are more reliable.

---
+++ ShipLootPlus/ShipLootPlus/Patches/DepositItemsDeskPatcher.cs
@@ -0,0 +1,16 @@
+﻿using GameNetcodeStuff;
+using HarmonyLib;
+using ShipLootPlus.Utils;
+
+namespace ShipLootPlus.Patches
+{
+    internal class DepositItemsDeskPatcher
+    {
+        [HarmonyPatch(typeof(DepositItemsDesk), nameof(PlaceItemOnCounter))]
+        [HarmonyPostfix]
+        private static void PlaceItemOnCounter(PlayerControllerB __instance)
+        {
+            UiHelper.RefreshElementValues();
+        }
+    }
+}
---
+++ ShipLootPlus/ShipLootPlus/Patches/GrabbableObjectPatcher.cs
@@ -0,0 +1,18 @@
+﻿using HarmonyLib;
+using ShipLootPlus.Utils;
+
+namespace ShipLootPlus.Patches
+{
+    internal class GrabbableObjectPatcher
+    {
+        [HarmonyPatch(typeof(GrabbableObject), nameof(OnHitGround))]
+        [HarmonyPostfix]
+        private static void OnHitGround(GrabbableObject __instance)
+        {
+            if (__instance.itemProperties.isScrap && !UiHelper.IsRefreshing)
+            {
+                GameNetworkManager.Instance.StartCoroutine(UiHelper.UpdateDatapoints());
+            }
+        }
+    }
+}
